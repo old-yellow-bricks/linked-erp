5d3cada61eb54c5ee2965d4380ee9babb383c630
ArthurSoave <atrpaula@gmail.com>
2022-11-05 16:04:31 -0300

    first commit

---
+++ src/main/kotlin/com/oybricks/linkederp/LinkederpApplication.kt
@@ -0,0 +1,11 @@
+package com.oybricks.linkederp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LinkederpApplication
+
+fun main(args: Array<String>) {
+    runApplication<LinkederpApplication>(*args)
+}
---
+++ src/test/kotlin/com/oybricks/linkederp/LinkederpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oybricks.linkederp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LinkederpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
